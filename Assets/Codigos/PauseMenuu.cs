ab157ec2aa36d0781a3d5f3cc5848b703e087bbe
matiespinel <matiasespinel.me@gmail.com>
2022-10-29 20:38:56 -0300

    hay menu de pausa y la barra de volumen controla el sonido

--- Assets/Codigos/PauseMenuu.cs
+++ Assets/Codigos/PauseMenuu.cs
@@ -8,20 +8,7 @@ public class PauseMenuu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI;
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                Pause();
-            }
-        }
-    }
+  
 
     public void Resume()
     {
