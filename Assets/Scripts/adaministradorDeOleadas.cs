4791f3926c7d24c72801dd7968cbbe461e4fd365
Martin Berestovoy <martin.berestovoy@gmail.com>
2022-11-29 14:21:37 -0300

    tumba

--- Assets/Scripts/adaministradorDeOleadas.cs
+++ Assets/Scripts/adaministradorDeOleadas.cs
@@ -12,11 +12,28 @@ public class adaministradorDeOleadas : MonoBehaviour
     private AudioSource audioSource;
     [SerializeField] private AudioClip apagarTorre;
 
+    public GameObject tumba;
+    public GameObject tumba2;
+    public GameObject tumba3;
+    public GameObject tumba4;
+    public GameObject tumba5;
+    public GameObject tumba6;
+    public GameObject tumba7;
+    public GameObject tumba8;
+
     public void ActivateAllEnemies()
     {
         for (int i = 0; i < enemigos.Length; i++)
         {
             enemigos[i].SetActive(true);
+            tumba.SetActive(false);
+            tumba2.SetActive(false);
+            tumba3.SetActive(false);
+            tumba4.SetActive(false);
+            tumba5.SetActive(false);
+            tumba6.SetActive(false);
+            tumba7.SetActive(false);
+            tumba8.SetActive(false);
         }
         laserTorre.SetActive(true);
         waveCount = 0;
